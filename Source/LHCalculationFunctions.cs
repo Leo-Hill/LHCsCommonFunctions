937aaa665b101f42575a6c22c7ebc2c4b4840f00
LeoHill <Leonhard.Hillmeier@gmx.de>
2021-05-17 00:07:42 +0200

    20210517

--- Source/LHCalculationFunctions.cs
+++ Source/LHCalculationFunctions.cs
@@ -13,15 +13,21 @@ namespace LHCommonFunctions.Source
     **********************************************************************************************/
     public static class LHCalculationFunctions
     {
-        //This function calculates the CRC checksum of an u32 array
-        public static UInt32 u32CalculateCrc(UInt32[] qaData, int qiSize)
+        //This function calculates the CRC32 checksum of a byte array
+        public static UInt32 u32CalculateCrc(byte[] qaData)
         {
+            if (0 != (qaData.Length % 4))   //Check if the array can be divided by 4 in order to seperate the bytes to U32s
+            {
+                throw new Exception("CRC32 exception. Array can non be converted!");
+            }
             UInt32 u32ActCrcValue = 0xFFFFFFFF;                                                     //Initial CRC value
             UInt32 u32CrcPoly = 0x4C11DB7;                                                          //Crc polynom
             UInt32 u32ActBinaryIndex;                                                               //Current bit of the crc value processing
-            for (int iByteCnt = 0; iByteCnt < qiSize; iByteCnt++)
+            UInt32 u32ActInputData;
+            for (int iByteCnt = 0; iByteCnt < qaData.Length; iByteCnt+=4)
             {
-                u32ActCrcValue = u32ActCrcValue ^ qaData[iByteCnt];
+                u32ActInputData = BitConverter.ToUInt32(qaData, iByteCnt);  //Convert 4 bytes to 1 u32
+                u32ActCrcValue = u32ActCrcValue ^ u32ActInputData;
                 u32ActBinaryIndex = 0;
                 while (u32ActBinaryIndex < 32)
                 {
