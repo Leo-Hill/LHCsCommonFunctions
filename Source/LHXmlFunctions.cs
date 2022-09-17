b3bb6e9d867bc6f96e582cce1062947f4f703917
LeoHill <Leonhard.Hillmeier@gmx.de>
2022-09-17 21:27:04 +0200

    Removed unused xml function

--- Source/LHXmlFunctions.cs
+++ Source/LHXmlFunctions.cs
@@ -14,25 +14,19 @@ namespace LHCommonFunctions.Source
     **********************************************************************************************/
     public static class LHXmlFunctions
     {
-
-        //This function creates a new xml file with an empty "root" node 
-        public static void vCreateXmlSettingsFile(String qsPath, String qsFilename)
-        {
-            XDocument XDExport = new XDocument();                                                   //Document to export
-            XElement XERoot = new XElement("root");
-            XDExport.Add(XERoot);
-            XDExport.Save(qsPath + "\\" + qsFilename);                                              //Save the file 
-        }
+        private const String DICT_XML_ROOT_ELEMENT_NAME = "root";
+        private const String DICT_XML_ITEM_ELEMENT_NAME = "item";
+        private const String DICT_XML_ID_TAG = "id";
 
         //This function converts a sorted dictionary to a xml file and exports the file
         public static void vExportDictionaryToXml(SortedDictionary<String, String> qSDExport, String qsPath, String qsFilename)
         {
             XDocument XDExport = new XDocument();                                                   //Document to export
-            XElement XERoot = new XElement("root");
+            XElement XERoot = new XElement(DICT_XML_ROOT_ELEMENT_NAME);
             foreach (String sKey in qSDExport.Keys)
             {
-                XElement XEInsert = new XElement("item", qSDExport[sKey]);
-                XEInsert.SetAttributeValue("id", sKey);
+                XElement XEInsert = new XElement(DICT_XML_ITEM_ELEMENT_NAME, qSDExport[sKey]);
+                XEInsert.SetAttributeValue(DICT_XML_ID_TAG, sKey);
                 XERoot.Add(XEInsert);
             }
             XDExport.Add(XERoot);
@@ -45,9 +39,9 @@ namespace LHCommonFunctions.Source
         {
             XDocument XDImport = XDocument.Load(qsPath + "\\" + qsFilename);                        //Load the xml file
             SortedDictionary<String, String> SDReturn = new SortedDictionary<String, String>();
-            foreach (XElement actElement in XDImport.Descendants("item"))                           //Loop through xml childs 
+            foreach (XElement actElement in XDImport.Descendants(DICT_XML_ITEM_ELEMENT_NAME))                           //Loop through xml childs 
             {
-                String sID = actElement.Attribute("id").Value;
+                String sID = actElement.Attribute(DICT_XML_ID_TAG).Value;
                 String sValue = actElement.Value;
                 SDReturn.Add(sID, sValue);
             }
